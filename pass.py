11821c59a9efcefb720741f2051cdc3d711fe5df
Sony Podrazska <146202175+Podrazik@users.noreply.github.com>
2023-10-06 17:24:26 +0300

    Create pass.py

---
+++ pass.py
@@ -0,0 +1,12 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QHBoxLayout, QMessageBox
+from random import *
+
+app = QApplication([])
+main_win = QWidget()
+
+main.win.resize(700,500)
+main_win.setWindowTitle('The password game')
+
+main_layout = QVBoxLayout()
+
